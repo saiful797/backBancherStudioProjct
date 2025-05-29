4ee55decdd1fd159dc6f14c4623327f13db3b716
Saiful Islam <saifulislamsaif797@gmail.com>
2025-05-29 14:29:34 +0600

    Developed the single product section

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
+// Header section JS here
 const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 
 menuToggle.addEventListener("click", () => {
   mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
 });
+
+
+
+// Single product Section JS Here
+// Thumbnail image switching
+  function changeImage(thumbnail) {
+    document.getElementById('mainProductImage').src = thumbnail.src;
+    document.querySelectorAll('.single-product-thumbnail').forEach(thumb => thumb.classList.remove('selected'));
+    thumbnail.classList.add('selected');
+  }
